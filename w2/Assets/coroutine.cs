385fdd0861e58ddc5cba2e041057c0895bc97bcd
unknown <piya6662009@hotmail.com>
2015-09-10 03:00:15 +0700

    week2work

---
+++ w2/Assets/coroutine.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coroutine : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (WaitAndPrint());
+		Debug.Log("Hello" + Time.time);
+		StartCoroutine (Example());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	IEnumerator WaitAndPrint() {
+		yield return new WaitForSeconds(5f);
+		print("WaitAndPrint " + Time.time);
+	}
+	IEnumerator Example() {
+		print("Starting " + Time.time);
+		yield return WaitAndPrint();
+		yield return new WaitForSeconds(7f);
+		print("Done " + Time.time);
+		Debug.Log("Hello" + Time.time);
+		yield return new WaitForSeconds(10);
+		Application.LoadLevel("2");
+	}
+	
+	
+}
+
